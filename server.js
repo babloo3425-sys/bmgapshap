c2cf358ee118d59c57b471f50861b2446d5df8c1
bablu kumar <52916568-babloo3425@users.noreply.replit.com>
2026-02-02 08:45:06 +0000

    add base server and project setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log("BmGapShap running on port", PORT);
+});
